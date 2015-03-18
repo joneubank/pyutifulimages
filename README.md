3e758b58086172067c25baed6ab8a63c2441ac0b
joneubank <joneubank@gmail.com>
2015-03-17 23:07:02 -0400

    imgutil.py added for convenience methods - save() included as a first example. README updated to reflect dependency.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pyutifulimages
 Programatic image generation in python. Sandbox project, no objective or application.
+
+##Requirements
+The following python packages are dependencies for this package to run:
+* [pillow](https://pypi.python.org/pypi/Pillow/2.0.0) - Maintained fork of PIL (Python Image Library)
---
+++ imgutil.py
@@ -0,0 +1,28 @@
+import os, sys
+from PIL import Image
+
+# Save a PIL Image at the given path. Ensure that the filetype is specified.
+def save(image, path, name):
+	
+	#first ensure the path exists, and create if it doesn't
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+    #Now save image, catching any errors which occur
+	try:
+		filename = path + name
+		image.save(filename)
+	except:
+		print "An error occured, details follow, consider correcting for this:"
+		print str(sys.exc_info()[0])
+		raise
+
+
+
+def main():
+	print "Not really any reason to run this, but thanks for playing!"
+
+
+
+if __name__=='__main__':
+	main()

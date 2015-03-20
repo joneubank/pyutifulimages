d47c1296cb10f4c380c5cce164c28aa311c8f24c
Jon Eubank <joneubank@gmail.com>
2015-03-20 13:25:31 -0400

    Updated boxdraw and colorz to meet PEP8 standards

--- imgutil.py
+++ imgutil.py
@@ -1,28 +1,30 @@
 import os
 import sys
-import random
-from PIL import Image
+# import random
+# from PIL import Image
+
 
 # Save a PIL Image at the given path. Ensure that the filetype is specified.
 def save(image, path, name):
-	
-	#first ensure the path exists, and create if it doesn't
-	if not os.path.exists(path):
-		os.makedirs(path)
 
-	#Now save image, catching any errors which occur
-	try:
-		filename = path + name
-		image.save(filename)
-	except:
-		#temporary except block. as real errors are discovered we can handle those issues specifically.
-		print "An error occured, details follow, consider correcting for this:"
-		print str(sys.exc_info()[0])
-		raise
+    # first ensure the path exists, and create if it doesn't
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    # Now save image, catching any errors which occur
+    try:
+        filename = path + name
+        image.save(filename)
+    except:
+        # temporary except block. handle specific errors as they occur
+        print "An error occured, details follow, consider correcting for this:"
+        print str(sys.exc_info()[0])
+        raise
+
 
 def main():
-	print "Not really any reason to run this, but thanks for playing!"
+    print "Not really any reason to run this, but thanks for playing!"
 
 
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()

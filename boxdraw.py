2cb423b14feeec213ffe3941363664f20b3d462a
joneubank <joneubank@gmail.com>
2015-03-19 21:44:03 -0400

    added boxdraw.py (image generations), renamed color.py to colorz

---
+++ boxdraw.py
@@ -0,0 +1,16 @@
+import random
+import imgutil
+import color
+from color import Color
+
+
+
+
+
+def main():
+	# print color.randcolor().rgb()
+	# print color.fromNum(random.randint(0, 256*256*256)).rgb()
+	pass
+
+if __name__ == '__main__':
+	main()

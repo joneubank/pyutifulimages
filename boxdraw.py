4af6f6cba894d63c7daf8b5cb3df87d4cfd49c39
Jon Eubank <joneubank@gmail.com>
2015-03-20 13:23:57 -0400

    Updated boxdraw and colorz to meet PEP8 standards

--- boxdraw.py
+++ boxdraw.py
@@ -1,40 +1,47 @@
-import random
+# import random
 import imgutil
 import colorz
-from color import Color
+# from color import Color
 
 import time
 
 from PIL import Image
-from PIL import ImageColor
+# from PIL import ImageColor
 from PIL import ImageDraw
 
+
 def makeBlank(width, height, color=colorz.std['BLACK']):
-	return Image.new("RGBA", (width, height), color.rgb())
+    return Image.new("RGBA", (width, height), color.rgb())
+
 
 # tilesWide is number of tiles horizontaly
 # tilesHigh is number of tiles veritcally
 # tileSize is pixels per tile, both horizontal and vertical
 def makeSquareGrid(tilesWide, tilesHigh, tileSize):
-	width = tilesWide*tileSize
-	height = tilesHigh*tileSize
+    width = tilesWide*tileSize
+    height = tilesHigh*tileSize
 
-	im = makeBlank(width, height)
-	draw = ImageDraw.Draw(im,"RGBA")
+    im = makeBlank(width, height)
+    draw = ImageDraw.Draw(im, "RGBA")
 
-	for x in range(0,tilesWide):
-		for y in range(0,tilesHigh):
-			r = [x*tileSize,y*tileSize,(x+1)*tileSize,(y+1)*tileSize]
-			draw.rectangle(r,fill=colorz.randcolor(min=20,max=235).rgba())
+    for x in range(0, tilesWide):
+        for y in range(0, tilesHigh):
+            r = [x*tileSize, y*tileSize, (x+1)*tileSize, (y+1)*tileSize]
+            draw.rectangle(r, fill=colorz.randcolor(min=20, max=235).rgba())
 
-	return im
+    return im
+
 
 def main():
-	
-	# im = makeSquareGrid(1000,1000,1)
-	# imgutil.save(im,"created/grid/",str(time.strftime("%Y%m%d%H%M%S")) + ".png")
 
-	pass
+    im = makeSquareGrid(12, 12, 85)
+    imgutil.save(
+        im,
+        "created/grid/",
+        str(time.strftime("%Y%m%d%H%M%S")) + ".png"
+        )
+
+    pass
 
 if __name__ == '__main__':
-	main()
+    main()
--- colorz.py
+++ colorz.py
@@ -1,52 +1,54 @@
 import random
 
+
 class Color:
+    # Optional:
+    # a=255 - specify opacity if transparency is desired
+    def __init__(self, r, g, b, a=255):
+        self.r = clamp(r)
+        self.g = clamp(g)
+        self.b = clamp(b)
+        self.a = clamp(a)
 
-	# Optional:
-	# a=255 - provide (between 0-255) if you want there to set opacity as somthing other than 255
-	def __init__(self, r, g, b, a=255):
-		self.r = clamp(r)
-		self.g = clamp(g)
-		self.b = clamp(b)
-		self.a = clamp(a)
+    def rgb(self):
+        return (self.r, self.g, self.b)
 
-	def rgb(self):
-		return (self.r, self.g, self.b)
+    def rgba(self):
+        return (self.r, self.g, self.b, self.a)
 
-	def rgba(self):
-		return (self.r, self.g, self.b, self.a)
 
 def fromNum(num):
-	r = num&255
-	g = (num&(255*256)) >> 8
-	b = (num&(255*256*256)) >> 16
+    r = num & 255
+    g = (num & (255*256)) >> 8
+    b = (num & (255*256*256)) >> 16
 
-	#Apply clamps
-	r = clamp(r)
-	g = clamp(g)
-	b = clamp(b)
+    # Apply clamps
+    r = clamp(r)
+    g = clamp(g)
+    b = clamp(b)
 
-	return Color(r, g, b)
-
+    return Color(r, g, b)
 
 
 # Optional:
-# min=0 - Specify minimum generated value - applies to R, G, B, and A values (A only if opacity set outside -1 to 255)
-# max=255 - Specify maximum generated value - applies to R, G, B, and A values (A only if opacity set outside -1 to 255)
+# min=0 - Specify minimum generated value - applies to R, G, B, and A values
+# max=255 - Specify maximum generated value - applies to R, G, B, and A values
 # opacity=255 - specify opacity or set to -1 for random
 def randcolor(min=0, max=255, opacity=255):
-	r = random.randint(min, max)
-	g = random.randint(min, max)
-	b = random.randint(min, max)
+    r = random.randint(min, max)
+    g = random.randint(min, max)
+    b = random.randint(min, max)
 
-	if(opacity>=0 and opacity<=255):
-		a = opacity
-	else:
-		a = random.randint(min,max)
+    if(opacity >= 0 and opacity <= 255):
+        a = opacity
+    else:
+        a = random.randint(min, max)
 
-	return Color(r,g,b,a)
+    return Color(r, g, b, a)
 
-#Returns input limited to rang 0-255, if higher returns 255, if lower returns 0
+
+# Returns input limited to rang 0-255
+# if higher returns 255, if lower returns 0
 def clamp(n):
     if n < 0:
         return 0
@@ -56,16 +58,17 @@ def clamp(n):
         return n
 
 std = {
-	'BLACK'		:Color(  0,   0,   0),
-	'WHITE'		:Color(255, 255, 255),
-	'RED'		:Color(255,   0,   0),
-	'GREEN'		:Color(  0, 255,   0),
-	'BLUE'		:Color(  0,   0, 255)
-	}
+    'BLACK'		:Color(  0,   0,   0),
+    'WHITE'		:Color(255, 255, 255),
+    'RED'		:Color(255,   0,   0),
+    'GREEN'		:Color(  0, 255,   0),
+    'BLUE'		:Color(  0,   0, 255)
+    }
+
 
 def main():
-	# print colorFromNum(300000).rgb()
-	pass
+    # print colorFromNum(300000).rgb()
+    pass
 
 if __name__ == '__main__':
-	main()
+    main()

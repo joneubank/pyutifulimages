fb88f630284a6c376bde6975c738cafa2481a484
joneubank <joneubank@gmail.com>
2015-03-23 21:37:15 -0400

    moved colourlovers code into pallettes

--- pallettes.py
+++ pallettes.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup
+import urllib2
+
 from colorz import Color
 import colorz
 
@@ -36,6 +39,54 @@ def randpallette(num):
     for i in range(0, num):
         output.append(colorz.randcolor())
     return output
+
+# ############################################
+# This section contains code for getting 
+# pallettes from the site www.colourlovers.com
+# --------------------------------------------
+
+BASEURL_PALLETTE = "http://www.colourlovers.com/palette/"
+
+
+def httpGetUrl(url):
+    attempts = 0
+    while attempts < 10:
+        try:
+            print "pyutifulimages.colourlovers.py - Attempting to get url: " + url
+
+            response = urllib2.urlopen(url)
+            html = response.read()
+            print("httpGetUrl success")
+            return html
+        except urllib2.HTTPError as e:
+            print("HTTPError occured: " + str(e.code))
+            raise
+        except urllib2.URLError as e:
+            print("URL Error occured: " + str(e.reason))
+            raise
+
+# returns pallette with colors from html
+def parsePalletteColors(html):
+    output = []
+    
+    dom = BeautifulSoup(html)
+    
+    colorWrappers = dom('div',{'class':'col-80'})
+    for cw in colorWrappers:
+        colorInfoString = str(cw('h4')[0])
+        colorInfo = colorInfoString.partition('>')[2].partition('<')[0].split(',')
+        c = Color(int(colorInfo[0]), int(colorInfo[1]), int(colorInfo[2]))
+        output.append(c)
+
+    return output
+
+
+def getFromColourLovers(num):
+    url = BASEURL_PALLETTE + str(num)
+    html = httpGetUrl(url)
+    pallette = parsePalletteColors(html)
+    return pallette
+
 
 
 def main():
--- wallpaper.py
+++ wallpaper.py
@@ -4,29 +4,7 @@ import time
 import boxdraw
 import pallettes
 import imgutil
-import colourlovers
 import random
-
-# Divisors of 2560
-# {1, 2, 4, 5, 8, 10, 16, 20, 32, 40, 64, 80, 128, 160, 256, 320, 512, 640, 1280, 2560}
-# Divisors of 1600
-# {1, 2, 4, 5, 8, 10, 16, 20, 25, 32, 40, 50, 64, 80, 100, 160, 200, 320, 400, 800, 1600}
-#
-# Common Divisors:
-# [1, 2, 4, 5, 8, 16, 20, 25, 32, 40, 64, 80, 160, 320]
-#
-# Grid Box Combination Options:
-# (8,5,320),
-# (16,10,160), 
-# (32,20,80),
-# (102,,64),
-# 
-# (128,80,20),
-# (160,100,16),
-# (320,200,8),
-# (512,320,5),
-# (640,400,4),
-# (1280,800,2)
 
 SIZES_2560 = [320, 160, 80, 64, 40, 32, ]
 
@@ -39,8 +17,8 @@ def main():
     # pallette = colourlovers.getPallette(random.randint(1,3687000))
 
     #Next line gets a specific pallette from color lovers
-    colourloverid = 125448
-    pallette = colourlovers.getPallette(colourloverid)
+    colourloverid = 1283145
+    pallette = pallettes.getFromColourLovers(colourloverid)
 
     # 2560 / Standard
     # makeGridPallette(pallette,80, path="created/wallpaper/")
@@ -48,7 +26,7 @@ def main():
     # 1920x1080
     # makeGridPallette(pallette,120, path="created/wallpaper/", width=1920, height=1080)
 
-    makeGridPallette(pallette,160, path="created/wallpaper/", width=2560, height=2560, variance=20, name=(str(time.strftime("%Y%m%d%H%M%S"))+"_"+str(colourloverid)+".png"))
+    makeGridPallette(pallette,320, path="created/wallpaper/", width=2560, height=1920, variance=15, name=(str(time.strftime("%Y%m%d%H%M%S"))+"_"+str(colourloverid)+".png"))
 
 if __name__ == '__main__':
     main()
